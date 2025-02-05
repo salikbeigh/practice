e29bdef3f8898da132d7363624296253f52990ac
salikbeigh <7006828611>
2025-02-05 19:02:02 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,32 +1,28 @@
 import React, { useRef, useState } from "react";
 
 const App = () => {
-  const inputRef = useRef(null);
   const [todos, setTodos] = useState([]);
+  const inputRef = useRef(null);
   const [count, setCount] = useState(1);
 
   const add = () => {
-    setTodos([
-      ...todos,
-      { id: count, text: inputRef.current.value, display: "" },
-    ]);
-    setCount(count + 1);
+    setTodos([...todos, { text: inputRef.current.value, id: count }]);
     inputRef.current.value = "";
+    setCount(count + 1);
   };
-  const deleteTodo = (id) => {
+  const deleteTodos = (id) => {
     setTodos(todos.filter((item) => item.id != id));
   };
-
   return (
     <div>
       <input ref={inputRef} type="text" />
       <button onClick={add}>ADD</button>
       <div>
-        {todos.map((item, index) => {
+        {todos.map((item) => {
           return (
-            <li key={index} className="list-none">
-              {item.id} {item.text}{" "}
-              <button onClick={() => deleteTodo(item.id)}>❌</button>
+            <li className="list-none" key={item.id}>
+              {item.id}:{item.text}{" "}
+              <button onClick={() => deleteTodos(item.id)}>⤬</button>
             </li>
           );
         })}
